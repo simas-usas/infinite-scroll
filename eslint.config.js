1a3aa2d6040854760fbde5a6142a348053f3c034
simas.usas <simas.usas@zenitech.co.uk>
2024-08-19 15:45:50 +0300

    fixed issues with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,20 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    plugins: { react: pluginReact },
+    settings: {
+      react: {
+        version: "detect", 
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ];
